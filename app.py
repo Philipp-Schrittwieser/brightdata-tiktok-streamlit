2995bc0a18b9862b2afecd30437a78c687015ab6
Philipp Schrittwieser <office@sprechturbo.at>
2025-03-31 13:22:30 +0200

    w

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ st.markdown("""
               .st-emotion-cache-1p1m4ay {display: none;}
               .stActionButton {display: none;}
               ._viewerBadge_nim44_23 {display: none;}
+              ._link_gzau3_10 {display: none;}
               ._container_gzau3_1 {display: none;}
             """, unsafe_allow_html=True)
 
